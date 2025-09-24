b7a774d1b7828a9991b55ef53af8a15ffe9d05ea
Jhoselin-Diaz <xvantx2@gmail.com>
2025-09-23 20:24:58 -0500

    Se agrega 2da actu

--- src/main/java/com/upc/data/jpa/api/Repository/ProveedorRepository.java
+++ src/main/java/com/upc/data/jpa/api/Repository/ProveedorRepository.java
@@ -1,11 +1,7 @@
 package com.upc.data.jpa.api.Repository;
 
-import com.upc.data.jpa.api.Model.Producto;
+import com.upc.data.jpa.api.Model.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto> findByNombreContainingIgnoreCase(String nombre);
-
+public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
 }
